2f6b08cf76fb19cb7a0e355a5a8bbb7ad135544b
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2021-01-20 14:41:05 +1300

    Added tweeting

--- gunnymotivates.py
+++ gunnymotivates.py
@@ -1,5 +1,16 @@
 import motivations
 import random
+import tweepy
+import secrets
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(secrets.APIkey, secrets.APIsecret)
+auth.set_access_token(secrets.AccessToken, secrets.AccessSecret)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
 
 tweet = random.randint(0, 63)
-print(motivations.tweets[tweet])
+api.update_status(motivations.tweets[tweet])
